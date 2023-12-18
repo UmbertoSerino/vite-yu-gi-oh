67f53c1b48ffe508f1caef6c8127c0ae87b8d2f8
UmbertoSerino <serumberto@gmail.com>
2023-12-18 23:12:40 +0100

    created API call with js, but problem with images (created an array with image links)

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import AppCardElements from './components/AppCardElements.vue';
+import { store } from '../src/js/store';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppMain
+    AppMain,
+    AppCardElements
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  created() {
+    this.getCharacters();
+  },
+  methods: {
+    getCharacters() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+        .then((response) => {
+          this.store.charactersList = response.data.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .finally(function () {
+        });
+    }
   }
 }
 </script>
 
 <template>
   <AppHeader />
-  <AppMain />
+  <main>
+    <AppCardElements />
+  </main>
 </template>
 
 <style lang="scss">
--- src/components/AppCardElements.vue
+++ src/components/AppCardElements.vue
@@ -1,22 +1,50 @@
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <article v-for="character in store.charactersList" :key="character.id">
+        <div>
+            <img v-for="imgUrl in imgList" :src="imgUrl[0]" alt="character.name">
+        </div>
+        <h2>{{ character.name }}</h2>
+    </article>
 </template>
+  
 <script>
+import { store } from '../js/store';
 export default {
     name: 'AppCardElements',
     data() {
         return {
+            store,
+            imgList: [
+                "https://images.ygoprodeck.com/images/cards/34541863.jpg",
+                "https://images.ygoprodeck.com/images/cards/64163367.jpg",
+                "https://images.ygoprodeck.com/images/cards/91231901.jpg",
+                "https://images.ygoprodeck.com/images/cards/73262676.jpg",
+                "https://images.ygoprodeck.com/images/cards/98319530.jpg",
+                "https://images.ygoprodeck.com/images/cards/37478723.jpg",
+                "https://images.ygoprodeck.com/images/cards/64867422.jpg",
+                "https://images.ygoprodeck.com/images/cards/90861137.jpg",
+                "https://images.ygoprodeck.com/images/cards/44256816.jpg",
+                "https://images.ygoprodeck.com/images/cards/86988864.jpg",
+                "https://images.ygoprodeck.com/images/cards/11714098.jpg",
+                "https://images.ygoprodeck.com/images/cards/83994646.jpg",
+                "https://images.ygoprodeck.com/images/cards/67048711.jpg",
+                "https://images.ygoprodeck.com/images/cards/23771716.jpg",
+                "https://images.ygoprodeck.com/images/cards/86198326.jpg",
+                "https://images.ygoprodeck.com/images/cards/14261867.jpg",
+                "https://images.ygoprodeck.com/images/cards/24140059.jpg",
+                "https://images.ygoprodeck.com/images/cards/6850209.jpg",
+                "https://images.ygoprodeck.com/images/cards/49140998.jpg",
+                "https://images.ygoprodeck.com/images/cards/68170903.jpg",
 
+            ]
         }
     },
-    methods: {
+};
 
-    },
-}
 </script>
+  
 <style lang="scss">
 @use '../style/partials/variables' as*;
 @use '../style/partials/mixins' as*;
 </style>
+  
